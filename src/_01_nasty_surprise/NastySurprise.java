f750e1c2e8afdb9aaf04c5a7ee711eae34034ce3
Christopher Tablas <Christopher.Tablasdelacruz@gompersstudent.org>
2020-01-13 14:23:11 -0800

    Nasty Surprise
    Simon Says

--- src/_01_nasty_surprise/NastySurprise.java
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -2,9 +2,14 @@ package _01_nasty_surprise;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class NastySurprise implements ActionListener {
@@ -15,6 +20,7 @@ public class NastySurprise implements ActionListener {
 	
 	NastySurprise(){
 		button.addActionListener(this);
+		button2.addActionListener(this);
 	
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
@@ -29,18 +35,32 @@ public class NastySurprise implements ActionListener {
 
 public static void main(String[] args) {
 	NastySurprise ns = new NastySurprise();
+	
 }
 
+private void showPictureFromTheInternet(String imageUrl) {
+    try {
+        URL url = new URL(imageUrl);
+        Icon icon = new ImageIcon(url);
+        JLabel imageLabel = new JLabel(icon);
+        JFrame frame = new JFrame();
+        frame.add(imageLabel);
+        frame.setVisible(true);
+        frame.pack();
+    } catch (MalformedURLException e) {
+        e.printStackTrace();
+    }
+}
 
 
 @Override
 public void actionPerformed(ActionEvent arg0) {
 	// TODO Auto-generated method stub
 	if(arg0.getSource()==button){
-		showPictureFromTheInternet();
+		showPictureFromTheInternet("https://cdn.royalcanin-weshare-online.io/pCJJPmYBaxEApS7LeAbn/v1/ed7h-how-to-buy-a-puppy-hero-dog");
 	}
 	if(arg0.getSource()==button2){
-		
+		showPictureFromTheInternet("https://www.tynker.com/projects/screenshot/5c82a87ff22e093b482857d1/scary-maze-game-1-1-1hi.png");
 	}
 }
 }
--- src/_02_simon_says/SimonSays.java
+++ src/_02_simon_says/SimonSays.java
@@ -27,75 +27,107 @@ public class SimonSays extends KeyAdapter {
 	private int tries = 0;
 	private boolean simonSays = false;
 	Date timeAtStart;
+	int Points = 0;
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-
+	JFrame frame;
 	public void run() {
 		// 2. Add the four images that match keyboard keys like this:
 		// images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
-
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching
 		// key when
 		// 'Simon says' otherwise press a different key"
-
+		JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key.");
 		// 4. Call the showImage method to show an image
-
+		showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
-
+		
 		// 16. If the keyCode matches the imageIndex and "Simon says"
-
+		
+		if(simonSays == true){	
+			if(e.getKeyCode() == imageIndex){
+				Points++;
+				System.out.println("CORRECT");}
+			
+			else{
+			System.out.println("INCORRECT");}
+		}	
+		
+		else{	
+			if(e.getKeyCode() != imageIndex){
+				Points++;
+				System.out.println("CORRECT");}
+			
+			else{
+				System.out.println("INCORRECT");}	
+		}
+	
 		// 17. Increase the value of score
-
+		
 		// 18. Use the speak method to tell the user they were correct
-
+		
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't
 		// say..."
-
+	
 		// 20. Increase the value of score
 
 		// 21. Use the speak method to tell the user they were correct
 
 		// 22. Increment tries by 1
-
+		tries++;
 		// 25. If tries is greater than 9 (or however many you want)...
-
+		if(tries > 9){
+			System.out.println(Points + "/10");
+			System.exit(0);
+		}
 		// 26. Tell the user their score
 
 		// 27. Exit the program
 
 		// 23. Dispose of the frame
-
+		frame.dispose();
 		// 24. Call the showImage method to show a new image
+		showImage();
 	}
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
-
+		frame = new JFrame();
 		// 6. Set the frame to visible
-
+		frame.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		// frame.add(getNextRandomImage());
-
+		frame.add(getNextRandomImage());
+		
 		// 8. Set the name of your frame
-
+		frame.setName("Simon Says");
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the defaultCloseOperation of your frame to
 		// JFrame.EXIT_ON_CLOSE
-
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+		frame.addKeyListener(this);
 		// 12. Create a new instance of Random
-
+		Random random = new Random();
 		// 13. Use the Random and the speak method to either say
 		// "Simon says press this key" or "Press this key"
-
+		simonSays = random.nextBoolean();
+		if(simonSays == true){
+			speak("Simon Says press this key");
+		}
+		else{
+			speak("press this key");
+		}
 		// 14. Above, set the value of simonSays to true/false appropriately
-
+		
 	}
 
 	private Component getNextRandomImage() {
@@ -110,11 +142,7 @@ public class SimonSays extends KeyAdapter {
 	}
 
 	void speak(String words) {
-		try {
-			Runtime.getRuntime().exec("say " + words).waitFor();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println(words);
 	}
 
 }
@@ -127,3 +155,4 @@ public class SimonSays extends KeyAdapter {
  * System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000);
  * System.exit(0);
  */
+
